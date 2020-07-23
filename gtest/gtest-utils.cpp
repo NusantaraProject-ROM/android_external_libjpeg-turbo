mirrorstogooglesource/libjpeg_turbo

Fix file path manipulation on Windows
File path manipulation code introduced in previous commits only
supported char_t[]-based POSIX pathnames. This commit adds support
for wchar_t[]-based Windows pathnames.

Bug: 993876
Change-Id: Ie51eb0a18063ad3209779d9a799309e7a7cd729e

gtest/gtest-utils.cpp
@@ -18,8 +18,16 @@
  * 3. This notice may not be removed or altered from any source distribution.
  */
 
+#include "base/strings/utf_string_conversions.h"
 #include "gtest-utils.h"
 
+#if defined(OS_WIN)
+std::wstring LGetTargetDirectory() {
+  base::FilePath path;
+  base::PathService::Get(base::DIR_CURRENT, &path);
+  return base::UTF8ToWide(path.MaybeAsASCII());
+}
+#else
 std::string GetTargetDirectory() {
 #if defined(ANDROID)
   return "/sdcard";
@@ -29,6 +37,7 @@ std::string GetTargetDirectory() {
   return path.MaybeAsASCII();
 #endif
 }
+#endif
 
 void GetTestFilePath(base::FilePath* path, const std::string filename) {
   ASSERT_TRUE(base::PathService::Get(base::DIR_SOURCE_ROOT, path));

gtest/gtest-utils.h
@@ -33,7 +33,11 @@
 // Returns the absolute path of the test output directory as a string.
 // On Android this path is /sdcard; on all other platforms it is the current
 // directory.
+#if defined(OS_WIN)
+std::wstring LGetTargetDirectory();
+#else
 std::string GetTargetDirectory();
+#endif
 
 // Files used as input for libjpeg-turbo unit tests are stored in
 // <chromium>/src/third_party/libjpeg_turbo/testimages.

NusantaraProject-ROM/android_external_libjpeg-turbo

gtest/gtest-utils.cpp
@@ -18,8 +18,16 @@
  * 3. This notice may not be removed or altered from any source distribution.
  */
 
+#include "base/strings/utf_string_conversions.h"
 #include "gtest-utils.h"
 
+#if defined(OS_WIN)
+std::wstring GetTargetDirectory() {
+  base::FilePath path;
+  base::PathService::Get(base::DIR_CURRENT, &path);
+  return base::UTF8ToWide(path.MaybeAsASCII());
+}
+#else
 std::string GetTargetDirectory() {
 #if defined(ANDROID)
   return "/sdcard";
@@ -29,6 +37,7 @@ std::string GetTargetDirectory() {
   return path.MaybeAsASCII();
 #endif
 }
+#endif
 
 void GetTestFilePath(base::FilePath* path, const std::string filename) {
   ASSERT_TRUE(base::PathService::Get(base::DIR_SOURCE_ROOT, path));

gtest/gtest-utils.h
@@ -33,7 +33,11 @@
 // Returns the absolute path of the test output directory as a string.
 // On Android this path is /sdcard; on all other platforms it is the current
 // directory.
+#if defined(OS_WIN)
+std::wstring GetTargetDirectory();
+#else
 std::string GetTargetDirectory();
+#endif
 
 // Files used as input for libjpeg-turbo unit tests are stored in
 // <chromium>/src/third_party/libjpeg_turbo/testimages.